7e036fe190a0d1625e38f0b1085f8bf9707b40b5
Santiagoo <santiagominotta2913@hotmail.com>
2026-09-10 16:45:40 -0500

    Create portfolio component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import About from './components/About/About'
+import Skills from './components/Skills/Skills'
+import Projects from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+
 function App() {
   return (
-    <h1>My Portfolio</h1>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return(
+        <nav>
+            <p>Navbar</p>
+        </nav>
+    )
+}
+
+export default Navbar
