e758b73259006d1a13dddb8e03dcc150aa0b8e5e
Santiagoo <santiagominotta2913@hotmail.com>
2026-09-15 14:14:52 -0500

    Set up hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,42 @@
+import './Hero.css'
+
 function Hero() {
     return (
-        <section>
-            <h1>Hero</h1>
+        <section className='hero' id='home'>
+            <div className="container hero__container">
+                <div className="hero__content">
+                    <p className='hero__eyebrow'>
+                        Hey, I'm Santiago!
+                    </p>
+
+                    <h1 className='hero__title'>
+                        Web
+                        <span> Developer</span>
+                    </h1>
+
+                    <div className="hero__actions">
+                        <a href="#projects" className='hero__button hero__button--primary'>
+                            View Projects
+                        </a>
+
+                        <a href="#contact" className='hero__button'>
+                            Contact Me
+                        </a>
+                    </div>
+                </div>
+
+                <div className="hero__visual">
+
+                    <div className="hero__image">
+                        <span>YOUR<br />IMAGE<br />HERE</span>
+                    </div>
+
+                    <div className="hero__code-card">
+                        <span></span>
+                        <p>Building for the web</p>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
